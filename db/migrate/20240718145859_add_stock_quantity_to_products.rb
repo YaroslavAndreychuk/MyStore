84b5fd60a42da09989e8b332227e693c72f0bfcf
Yaroslav Andreychuk <yaroslav.andreychuk@jetruby.com>
2024-07-18 18:30:53 +0300

    Task3 step 2

--- db/migrate/20240718145859_add_stock_quantity_to_products.rb
+++ db/migrate/20240718145859_add_stock_quantity_to_products.rb
@@ -1,5 +1,6 @@
 class AddStockQuantityToProducts < ActiveRecord::Migration[7.1]
   def change
     add_column :products, :stock_quantity, :integer
+    add_index :products, :stock_quantity
   end
 end
