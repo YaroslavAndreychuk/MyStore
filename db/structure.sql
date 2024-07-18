ae528d1909051f4d3221b5f0a33ee6d24f73552c
Yaroslav Andreychuk <yaroslav.andreychuk@jetruby.com>
2024-07-18 18:32:18 +0300

    Task3 step 3

--- db/structure.sql
+++ db/structure.sql
@@ -41,39 +41,6 @@ CREATE TABLE public.products (
 
 
 --
--- Name: products_copy; Type: TABLE; Schema: public; Owner: -
---
-
-CREATE TABLE public.products_copy (
-    id bigint NOT NULL,
-    name character varying NOT NULL,
-    description text,
-    price numeric(10,2) DEFAULT 0.0 NOT NULL,
-    created_at timestamp(6) without time zone NOT NULL,
-    updated_at timestamp(6) without time zone NOT NULL
-);
-
-
---
--- Name: products_copy_id_seq; Type: SEQUENCE; Schema: public; Owner: -
---
-
-CREATE SEQUENCE public.products_copy_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
---
--- Name: products_copy_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
---
-
-ALTER SEQUENCE public.products_copy_id_seq OWNED BY public.products_copy.id;
-
-
---
 -- Name: products_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
@@ -109,26 +76,11 @@ ALTER TABLE ONLY public.products ALTER COLUMN id SET DEFAULT nextval('public.pro
 
 
 --
--- Name: products_copy id; Type: DEFAULT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.products_copy ALTER COLUMN id SET DEFAULT nextval('public.products_copy_id_seq'::regclass);
-
-
---
 -- Name: ar_internal_metadata ar_internal_metadata_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.ar_internal_metadata
     ADD CONSTRAINT ar_internal_metadata_pkey PRIMARY KEY (key);
-
-
---
--- Name: products_copy products_copy_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.products_copy
-    ADD CONSTRAINT products_copy_pkey PRIMARY KEY (id);
 
 
 --
@@ -161,6 +113,7 @@ CREATE UNIQUE INDEX index_products_on_name ON public.products USING btree (name)
 SET search_path TO "$user", public;
 
 INSERT INTO "schema_migrations" (version) VALUES
+('20240718153139'),
 ('20240718152319'),
 ('20240718145917'),
 ('20240718145859'),
