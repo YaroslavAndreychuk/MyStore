3b6b049bcff872c522287e7e479cf8e43fa6b8ed
Yaroslav Andreychuk <yaroslav.andreychuk@jetruby.com>
2024-07-18 19:45:09 +0300

    Task6 step 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,21 @@
-# db/seeds.rb
+# Очистка базы данных
+puts "Cleaning database..."
+Category.delete_all
+Supplier.delete_all
+Product.delete_all
 
-# Create categories
+# Создание категорий и поставщиков
+puts "Creating categories and suppliers..."
 Category.create(name: 'Electronics')
 Category.create(name: 'Books')
 
-# Create suppliers
 Supplier.create(name: 'Supplier 1')
 Supplier.create(name: 'Supplier 2')
 
-# Create products
-Product.create(name: 'Product 1', description: 'Description 1', price: 100.0, stock_quantity: nil, category_id: Category.find_by(name: 'Electronics').id, supplier_id: Supplier.find_by(name: 'Supplier 1').id)
-Product.create(name: 'Product 2', description: nil, price: '200.0abc', stock_quantity: 20, category_id: Category.find_by(name: 'Books').id, supplier_id: Supplier.find_by(name: 'Supplier 2').id)
-Product.create(name: 'Product 3', description: 'Description 3', price: 222, stock_quantity: 30, category_id: nil, supplier_id: Supplier.find_by(name: 'Supplier 1').id)
+# Создание продуктов с разнообразными атрибутами
+puts "Creating products..."
+Product.create(name: 'Product 1', description: 'Description 1', price: 100.0, stock_quantity: nil, category_id: Category.first.id, supplier_id: Supplier.first.id)
+Product.create(name: 'Product 2', description: nil, price: 200.0, stock_quantity: 20, category_id: Category.second.id, supplier_id: Supplier.second.id)
+Product.create(name: 'Product 3', description: 'Description 3', price: 1, stock_quantity: 30, category_id: nil, supplier_id: Supplier.first.id)
+
+puts "Seeding finished."
